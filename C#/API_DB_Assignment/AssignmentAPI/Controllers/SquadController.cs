4ba7e444b9337e6172870dc0ea9191783a3b0ba4
Rohan P <Rohan.P@emtecinc.com>
2022-11-22 19:57:42 +0530

    Commit for ADO.NET and Dapper

---
+++ C#/API_DB_Assignment/AssignmentAPI/Controllers/SquadController.cs
@@ -0,0 +1,48 @@
+﻿
+namespace AssignmentAPI.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using AssignmentAPI.Models;
+    using AssignmentAPI.Repository;
+    using System.Threading.Tasks;
+
+    [ApiController]
+    [Route("[controller]")]
+
+    public class SquadController: ControllerBase
+    {
+        private readonly SquadRepository _squadRepository;
+
+        public SquadController(IConfiguration configuration)
+        {
+            _squadRepository = new SquadRepository(configuration);
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_squadRepository.PlayerDetails());
+        }
+       
+        //[HttpGet]
+        //public IActionResult GetDapper()
+        //{
+        //    return Ok(_squadRepository.Get());
+        //}
+        [HttpPost]
+        public IActionResult Post(Squad squad)
+        {
+            return Ok(_squadRepository.AddPlayer(squad));
+        }
+        [HttpPut]
+        public IActionResult Put(Squad squad, int id)
+        {
+            return Ok(_squadRepository.UpdatePlayerInfo(squad, id));
+        }
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            return Ok(_squadRepository.RemovePlayer(id));
+        }
+    }
+}
